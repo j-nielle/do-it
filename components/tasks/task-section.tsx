1cad2def4a3943174769c70ac9735ce4afb9ba04
j-nielle <rabayaangel@gmail.com>
2025-03-27 14:55:22 +0800

    feat: added droppable logic

--- components/tasks/task-section.tsx
+++ components/tasks/task-section.tsx
@@ -6,6 +6,7 @@ import TaskItem from "./task-item";
 import { ScrollArea } from "@/components/shadcn-ui/scroll-area";
 import { TaskStatus } from "@/lib/constants";
 import { Task } from "@/types/task";
+import { useDroppable } from "@dnd-kit/react";
 
 export default function TaskSection({ tasks }: { tasks: Task[] }) {
   return (
@@ -14,20 +15,34 @@ export default function TaskSection({ tasks }: { tasks: Task[] }) {
       <Card className="h-full" radius="sm">
         <TaskButtonGroup />
         <Divider />
-        <ScrollArea className="h-[253px]">
-          <CardBody className="flex flex-col gap-2">
-            {tasks?.map((task, index) => (
-              <TaskItem
-                key={task.id}
-                id={task.id}
-                index={index}
-                containerId={TaskStatus.BACKLOG}>
-                {task.title}
-              </TaskItem>
-            ))}
-          </CardBody>
-        </ScrollArea>
+        <BacklogColumn tasks={tasks} id={TaskStatus.BACKLOG} />
       </Card>
     </section>
   );
 }
+
+interface BacklogColumnProps {
+  tasks: Task[];
+  id: string;
+}
+
+const BacklogColumn = ({ tasks, id }: BacklogColumnProps) => {
+  const { ref } = useDroppable({
+    id,
+  });
+  return (
+    <ScrollArea ref={ref} className="h-[253px]">
+      <CardBody className="flex flex-col gap-2">
+        {tasks?.map((task, index) => (
+          <TaskItem
+            key={task.id}
+            id={task.id}
+            index={index}
+            containerId={TaskStatus.BACKLOG}>
+            {task.title}
+          </TaskItem>
+        ))}
+      </CardBody>
+    </ScrollArea>
+  );
+};
