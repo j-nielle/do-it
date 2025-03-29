e98839ba045159bcf0ad2ab60c85546d9be1e059
j-nielle <rabayaangel@gmail.com>
2025-03-30 00:52:33 +0800

    refactor: getTimeline()

--- lib/helpers/data.ts
+++ lib/helpers/data.ts
@@ -10,7 +10,7 @@ export const getStatusCounts = (tasks: Task[]) => {
       acc[lastStatus] = (acc[lastStatus] || 0) + 1;
       return acc;
     },
-    {} as Record<TaskStatus, number>,
+    {} as Record<TaskStatus, number>
   );
 
   const counts = statusOrder.map((status) => statusCounts[status] || 0);
@@ -19,4 +19,33 @@ export const getStatusCounts = (tasks: Task[]) => {
     statusCounts,
     counts,
   };
+};
+
+export const getTimeline = (tasks: Task[]) => {
+  return [
+    {
+      name: "Planned",
+      data: tasks
+        .filter((task) => task.timeline.planned)
+        .map((task) => ({
+          x: task.title,
+          y: [
+            task.timeline.planned?.start?.toDate().getTime() ?? 0,
+            task.timeline.planned?.end?.toDate().getTime() ?? 0,
+          ],
+        })),
+    },
+    {
+      name: "Actual",
+      data: tasks
+        .filter((task) => task.timeline.actualWorkPeriods?.length > 0)
+        .map((task) => ({
+          x: task.title,
+          y: [
+            task.timeline.actualWorkPeriods[0].start?.toDate().getTime() ?? 0,
+            task.timeline.actualWorkPeriods[0].end?.toDate().getTime() ?? 0,
+          ],
+        })),
+    },
+  ];
 };
