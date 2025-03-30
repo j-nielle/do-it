6845f1a455acd55b3cfd5ca2b17f9117412441c1
j-nielle <rabayaangel@gmail.com>
2025-03-30 19:15:50 +0800

    feat: initial error page

--- app/error.tsx
+++ app/error.tsx
@@ -12,18 +12,21 @@ export default function Error({
   useEffect(() => {
     // Log the error to an error reporting service
     /* eslint-disable no-console */
-    console.error(error);
+    console.error(error.message, error.name, error.cause, error.stack);
   }, [error]);
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <h2>
+        Something went wrong!{" "}
+        <span className="font-bold text-red-600">[{error.message}]</span>
+      </h2>
+      <p>{error.stack}</p>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
-        }
-      >
+        }>
         Try again
       </button>
     </div>
