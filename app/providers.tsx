391d743a5ca09ed49bac8d3eb043d2da76a093a7
j-nielle <rabayaangel@gmail.com>
2025-03-30 19:15:34 +0800

    chore: `npx heroui-cli@latest add toast`

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,6 +5,7 @@ import type { ThemeProviderProps } from "next-themes";
 import * as React from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { HeroUIProvider } from "@heroui/system";
+import { addToast, ToastProvider } from "@heroui/toast";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
@@ -26,6 +27,7 @@ export function Providers({ children, themeProps }: ProvidersProps) {
 
   return (
     <HeroUIProvider navigate={router.push}>
+      <ToastProvider />
       <Analytics />
       <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
     </HeroUIProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ const config = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/(button|calendar|card|code|date-picker|divider|form|image|input|kbd|link|listbox|modal|navbar|select|snippet|toggle|ripple|spinner|date-input|popover|scroll-shadow).js",
+    "./node_modules/@heroui/theme/dist/components/(button|calendar|card|code|date-picker|divider|form|image|input|kbd|link|listbox|modal|navbar|select|snippet|toggle|toast|ripple|spinner|date-input|popover|scroll-shadow).js",
   ],
   theme: {
     extend: {
