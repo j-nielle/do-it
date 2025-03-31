53bdc4f072a187a502c0065928efd2101c6ef64b
j-nielle <rabayaangel@gmail.com>
2025-03-31 18:03:58 +0800

    feat: new task card UI

--- components/pages/dashboard/tasks/task-backlog.tsx
+++ components/pages/dashboard/tasks/task-backlog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useDroppable } from "@dnd-kit/react";
 import { Card, CardBody } from "@heroui/card";
 
@@ -11,7 +13,6 @@ interface TaskBacklogProps {
   tasks: Task[];
   id: string;
 }
-
 export const TaskBacklog = ({ tasks, id }: TaskBacklogProps) => {
   const { ref } = useDroppable({
     id,
@@ -24,18 +25,17 @@ export const TaskBacklog = ({ tasks, id }: TaskBacklogProps) => {
           className="bg-slate-300/55 h-full dark:bg-slate-300/25"
           radius="sm"
           shadow="sm">
-          <CardBody>
+          <CardBody className="gap-2">
             <div className="mb-2 font-bold">Backlog</div>
             {tasks.length > 0 &&
               tasks?.map((task, index) => (
                 <TaskItem
-                  key={task.id}
-                  containerId={TaskStatus.BACKLOG}
                   id={task.id}
+                  key={task.id}
+                  task={task}
                   index={index}
-                  task={task}>
-                  {task.title}
-                </TaskItem>
+                  statusId={TS.BACKLOG}
+                />
               ))}
           </CardBody>
         </Card>
