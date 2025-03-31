e0c43a6dc1a9314bfb2768292b1b52d57e59c470
j-nielle <rabayaangel@gmail.com>
2025-03-31 18:00:26 +0800

    feat: new task card UI

--- components/pages/dashboard/tasks/task-item.tsx
+++ components/pages/dashboard/tasks/task-item.tsx
@@ -1,61 +1,71 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { Card, CardBody } from "@heroui/card";
 import { useSortable } from "@dnd-kit/react/sortable";
 
-import { afterDragUpdate } from "@/services/tasks";
 import { Task } from "@/types/task";
-import { TaskStatus } from "@/lib/constants/task";
-import { useToast } from "@/hooks/useToast";
-import { MoveIcon } from "@/components/icons";
+import { getLocalDateString } from "@/lib/helpers/date";
+import CategoryChip from '@/components/ui/task/category-chip';
+
+interface TaskItemProps {
+  id: string;
+  task: Task;
+  index: number;
+  statusId: string;
+}
 
 export default function TaskItem({
   id,
   task,
   index,
-  containerId,
-  children,
-}: {
-  id: string;
-  index: number;
-  containerId: string;
-  children?: React.ReactNode;
-  task: Task;
-}) {
-  const toast = useToast();
-  const { ref, sortable, isDropping, handleRef } = useSortable({
+  statusId: group,
+}: TaskItemProps) {
+  const { ref } = useSortable({
     id,
     index,
-    group: containerId,
+    group,
     type: "task",
     accept: ["task"],
     data: {
       type: "task",
-      task: task,
+      selectedStatus: group,
+      task,
     },
   });
-
-  const status = sortable.group as TaskStatus;
-  const sameStatus =
-    task.statusHistory[task.statusHistory.length - 1].status === status;
-
-  useEffect(() => {
-    if (isDropping && !sameStatus) {
-      toast(afterDragUpdate(id, status), "update");
-    }
-  }, [id, status, isDropping]);
 
   return (
     <Card ref={ref} className="cursor-grab" id={id} radius="sm" shadow="sm">
       <CardBody>
-        <div className="flex flex-row justify-start items-center gap-x-2">
-          <span
-            ref={handleRef}
-            className="p-1 opacity-65 active:opacity-100 *:opacity-65 *:active:opacity-100 dark:opacity-100">
-            <MoveIcon size={16} className="dark:text-white h-4 w-4 rotate-45" />
-          </span>
-          {children}
+        <div className="flex flex-col justify-start items-start gap-x-2 text-sm">
+          <p className="flex flex-row justify-between items-center w-full">
+            <span>{task.title}</span>{" "}
+            <CategoryChip category={task.category} />
+          </p>
+          {task.planned && (
+            <span>
+              <p>
+                Planned start:{" "}
+                {getLocalDateString(task.planned.start?.toDate()!!)}
+              </p>
+              <p>
+                Planned end: {getLocalDateString(task.planned.end?.toDate()!!)}
+              </p>
+            </span>
+          )}
+          {task.actual && (
+            <span>
+              <p>
+                Actual start:{" "}
+                {getLocalDateString(task.actual.start?.toDate()!!)}
+              </p>
+              {task.actual.end && (
+                <p>
+                  Actual end: {getLocalDateString(task.actual.end?.toDate())}
+                </p>
+              )}
+            </span>
+          )}
         </div>
       </CardBody>
     </Card>
