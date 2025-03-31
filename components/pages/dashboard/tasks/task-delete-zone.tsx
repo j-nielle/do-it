fe18569e77302c93bdfed482b0390ee6d97848d3
j-nielle <rabayaangel@gmail.com>
2025-03-31 18:02:28 +0800

    chore: updated styles

--- components/pages/dashboard/tasks/task-delete-zone.tsx
+++ components/pages/dashboard/tasks/task-delete-zone.tsx
@@ -1,6 +1,7 @@
+"use client";
+
 import { useDroppable } from "@dnd-kit/react";
-import { Button } from "@heroui/button";
-import { IconTrash } from "@tabler/icons-react";
+import { Card } from "@heroui/card";
 
 export default function TaskDeleteZone() {
   const { ref } = useDroppable({
@@ -8,18 +9,12 @@ export default function TaskDeleteZone() {
   });
 
   return (
-    <Button
+    <Card
       ref={ref}
-      isIconOnly
-      className="w-full md:w-fit bg-transparent !border-dashed !border-red-500 hover:!bg-red-500 hover:!text-white hover:!border-0"
-      color="danger"
-      variant="faded">
-      <span className="flex flex-row justify-between items-center gap-4">
-        <p className="md:hidden font-medium">Drop to delete</p>
-        <span className="hidden md:block">
-          <IconTrash size={16} />
-        </span>
-      </span>
-    </Button>
+      radius="sm"
+      shadow="sm"
+      className="bg-transparent border h-16 items-center justify-center text-red-500 border-dashed border-red-500">
+      <span className="font-medium text-center">Drop here to delete</span>
+    </Card>
   );
 }
